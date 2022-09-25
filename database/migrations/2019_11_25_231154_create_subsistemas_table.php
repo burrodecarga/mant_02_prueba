55b5562639804c41dbbfdd9b2b7c755b8257ef6f
burrodecarga <edwinedwinhenriquezh@hotmail.com>
2022-09-25 09:22:00 -0400

    respaldo 02

---
+++ database/migrations/2019_11_25_231154_create_subsistemas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubsistemasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subsistemas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('sistema_id')->unsigned()->default(1);
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->foreign('sistema_id')->references('id')->on('sistemas')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subsistemas');
+    }
+}
